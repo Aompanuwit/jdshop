56a20d0de80d50758fb9ecd22f5efe1ec391c0ca
AomPanuwit <tomtomza45@outlook.com>
2019-11-30 17:33:10 +0700

    Newproduct

--- saveproduct.php
+++ saveproduct.php
@@ -1,13 +1,31 @@
 <?php
+    include("connect.php");
+    echo ini_get("upload_max_filesize")."<br>";
     $allowedType=["jpg","jpeg","gif","png","tif","tiff"];
     $fileType=explode("/",$_FILES["filePic"]["type"]);
+    $size=$_FILES["filePic"]["size"];
     if(!in_array($fileType[1],$allowedType)){
         echo "Non-image file is not allowed.";
     }
+    else if($size>1.00){
+        echo "file size exceeds the maximun treshold.";
 
-    echo "Type:" . $_FILES["filePic"]["type"] . "<br>";
+    }
+
+    else{
+        $name = $_POST['txtName'];
+        $desc = $_POST['txtDescription'];
+        $price = $_POST['txtPrice'];
+        $unitInStock = $_POST['txtStock'];
+        $filename = $_FILES['txtStock']["name"];
+    /*echo "Type:" . $_FILES["filePic"]["type"] . "<br>";
     echo "Name:" . $_FILES["filePic"]["name"] . "<br>";
     echo "Size:" . $_FILES["filePic"]["size"] . "<br>";
     echo "Temp name:" . $_FILES["filePic"]["tmp_name"] . "<br>";
-    echo "Error:" . $_FILES["filePic"]["error"] . "<br>";
+    echo "Error:" . $_FILES["filePic"]["error"] . "<br>";*/
+
+    move_uploaded_file($_FILES["filePic"]["tmp_name"],"pig/". $_FILES["filePic"]["name"]);
+
+    $sqlInsert = "INSERT INTO product (name,description,price,nuitIuStock,picture)VALUES ('$name','$desc',$price,$unitInStock,'$filename')";
+    }
 ?>
